1bb98d8b4a4a9e205d71e8b178be9782a4f8e983
Alshlqany <moalshlkany@gmail.com>
2025-05-11 17:23:24 +0300

    fix: update import path for CartContext to match directory structure

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { CartContext } from '../../context/cartContext';
+import { CartContext } from "../../Context/cartContext";
 import logo from '../../assets/logo.png';
 import './BtnStyle.css';
 
