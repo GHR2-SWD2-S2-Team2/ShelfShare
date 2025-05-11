769cc45abd48bbeceb411729b5668a5fc17cc5e0
hadeer <hadeer.abelgawad44@gmail.com>
2025-05-11 19:35:53 +0300

    'update'

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
